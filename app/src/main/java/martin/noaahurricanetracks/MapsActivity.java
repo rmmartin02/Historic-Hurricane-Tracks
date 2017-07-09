0025a051c1bab36a7ed911fcaac447cdf1e3a062
Russell Martin <rmmartin02@email.wm.edu>
2017-07-09 15:47:06 -0400

    split activity between info and map working

--- app/src/main/java/martin/noaahurricanetracks/MapsActivity.java
+++ app/src/main/java/martin/noaahurricanetracks/MapsActivity.java
@@ -1,33 +1,23 @@
 package martin.noaahurricanetracks;
 
-import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.Typeface;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.RadioGroup;
-import android.widget.TextView;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity {
 
-    private GoogleMap mMap;
     private ArrayList<Hurricane> hurricaneList = new ArrayList<Hurricane>();
     private static final String TAG = MapsActivity.class.getSimpleName();
     private static final int EXTRATROP = Color.GRAY;
@@ -43,10 +33,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
 
         Scanner s = new Scanner(getResources().openRawResource(R.raw.stormdata));
         //Get selected basin from intent
@@ -95,84 +81,76 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         for(int i = 0; i<hurricaneList.size(); i++){
             Log.d(TAG, hurricaneList.get(i).toString());
         }
-    }
 
+        //get map fragment and ready it
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+                // Add track point markers
+                for(int i = 0; i<hurricaneList.size(); i++) {
+                    TrackPoint oldPoint = hurricaneList.get(i).getTrackPoints().get(0);
+                    for (int j = 1; j < hurricaneList.get(i).getTrackPoints().size(); j++) {
+                        TrackPoint point = hurricaneList.get(i).getTrackPoints().get(j);
+                        int dot = R.drawable.bluedot;
 
-        // Add track point markers
-        for(int i = 0; i<hurricaneList.size(); i++) {
-            TrackPoint oldPoint = hurricaneList.get(i).getTrackPoints().get(0);
-            for (int j = 1; j < hurricaneList.get(i).getTrackPoints().size(); j++) {
-                TrackPoint point = hurricaneList.get(i).getTrackPoints().get(j);
-                int dot = R.drawable.bluedot;
+                        //draw lines
+                        PolylineOptions polyLineOptions = new PolylineOptions();
+                        polyLineOptions.add(new LatLng(oldPoint.getLatitude(), oldPoint.getLongitude()));
+                        //decide color based on intensity
+                        Log.d(TAG, String.valueOf(point.getWind()));
+                        Log.d(TAG, point.getNature());
+                        polyLineOptions.color(TROPDEPR);
+                        if (point.getWind() >= 34.0) {
+                            polyLineOptions.color(TROPSTORM);
+                            dot = R.drawable.greendot;
+                        }
+                        if (point.getWind() >= 64.0) {
+                            polyLineOptions.color(CATONE);
+                            dot = R.drawable.yellowdot;
+                        }
+                        if (point.getWind() >= 83.0) {
+                            polyLineOptions.color(CATTWO);
+                            dot = R.drawable.orangedot;
+                        }
+                        if (point.getWind() >= 96.0) {
+                            polyLineOptions.color(CATTHREE);
+                            dot = R.drawable.reddot;
+                        }
+                        if (point.getWind() >= 113.0) {
+                            polyLineOptions.color(CATFOUR);
+                            dot = R.drawable.pinkdot;
+                        }
+                        if (point.getWind() >= 137.0){
+                            polyLineOptions.color(CATFIVE);
+                            dot = R.drawable.purpledot;
+                        }
+                        if (point.getNature().equals("ET")){
+                            polyLineOptions.color(EXTRATROP);
+                            dot = R.drawable.greydot;
+                        }
+                        if(point.getNature().equals("DS")) {
+                            polyLineOptions.color(TROPDEPR);
+                            dot = R.drawable.bluedot;
+                        }
+                        polyLineOptions.add(new LatLng(point.getLatitude(),point.getLongitude()));
+                        oldPoint = point;
+                        mMap.addPolyline(polyLineOptions);
 
-                //draw lines
-                PolylineOptions polyLineOptions = new PolylineOptions();
-                polyLineOptions.add(new LatLng(oldPoint.getLatitude(), oldPoint.getLongitude()));
-                //decide color based on intensity
-                Log.d(TAG, String.valueOf(point.getWind()));
-                Log.d(TAG, point.getNature());
-                polyLineOptions.color(TROPDEPR);
-                if (point.getWind() >= 34.0) {
-                    polyLineOptions.color(TROPSTORM);
-                    dot = R.drawable.greendot;
+                        mMap.addMarker(new MarkerOptions()
+                                .icon(BitmapDescriptorFactory.fromResource(dot))
+                                .position(new LatLng(point.getLatitude(),point.getLongitude()))
+                                .title(hurricaneList.get(i).getName())
+                                .snippet("" +
+                                        "Date: " + point.getISO_time() + "" +
+                                        "Wind(kt): " + point.getWind() + "" +
+                                        "Pressure(mb): " + point.getPressure() +
+                                        ""));
+                    }
                 }
-                if (point.getWind() >= 64.0) {
-                    polyLineOptions.color(CATONE);
-                    dot = R.drawable.yellowdot;
-                }
-                if (point.getWind() >= 83.0) {
-                    polyLineOptions.color(CATTWO);
-                    dot = R.drawable.orangedot;
-                }
-                if (point.getWind() >= 96.0) {
-                    polyLineOptions.color(CATTHREE);
-                    dot = R.drawable.reddot;
-                }
-                if (point.getWind() >= 113.0) {
-                    polyLineOptions.color(CATFOUR);
-                    dot = R.drawable.pinkdot;
-                }
-                if (point.getWind() >= 137.0){
-                    polyLineOptions.color(CATFIVE);
-                    dot = R.drawable.purpledot;
-                }
-                if (point.getNature().equals("ET")){
-                    polyLineOptions.color(EXTRATROP);
-                    dot = R.drawable.greydot;
-                }
-                if(point.getNature().equals("DS")) {
-                    polyLineOptions.color(TROPDEPR);
-                    dot = R.drawable.bluedot;
-                }
-                polyLineOptions.add(new LatLng(point.getLatitude(),point.getLongitude()));
-                oldPoint = point;
-                mMap.addPolyline(polyLineOptions);
-
-                mMap.addMarker(new MarkerOptions()
-                        .icon(BitmapDescriptorFactory.fromResource(dot))
-                        .position(new LatLng(point.getLatitude(),point.getLongitude()))
-                        .title(hurricaneList.get(i).getName())
-                        .snippet("" +
-                                "Date: " + point.getISO_time() + "" +
-                                "Wind(kt): " + point.getWind() + "" +
-                                "Pressure(mb): " + point.getPressure() +
-                                ""));
             }
-        }
+        });
     }
-
     //https://github.com/googlemaps/android-samples/blob/master/ApiDemos/app/src/main/java/com/example/mapdemo/MarkerDemoActivity.java
 }
